e7bcc1229c5a6fe8c94a823bb3fd1bb248db2531
Ezedev <ezesil96@gmail.com>
2023-12-05 18:42:33 -0300

    Archivos ordenados

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+  import ProductList from '../components/ProductList.vue'
 </script>
 
 <template>
-  <main>
-    HomeView
-  </main>
+    <ProductList/>
 </template>
