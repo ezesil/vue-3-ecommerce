8d18115ddca4caa51fe5cd4914be5d2482621e47
Ezedev <ezesil96@gmail.com>
2024-01-04 23:36:19 -0300

    Eliminado archivo de prettier e indentación

--- src/stores/ProductsStore.ts
+++ src/stores/ProductsStore.ts
@@ -2,22 +2,22 @@ import { defineStore } from 'pinia'
 import { type Product } from '../models/Product'
 
 const productss:Array<Product> = [
-    { id: 1, name: "Silla", price: 56}, 
-    { id: 2, name: "Monitor", price: 450},
-    { id: 3, name: "Microfono", price: 20},
-    { id: 4, name: "Silla", price: 568}, 
-    { id: 5, name: "Monitor", price: 600},
-    { id: 6, name: "Microfono", price: 40}
+  { id: 1, name: "Silla", price: 56}, 
+  { id: 2, name: "Monitor", price: 450},
+  { id: 3, name: "Microfono", price: 20},
+  { id: 4, name: "Silla", price: 568}, 
+  { id: 5, name: "Monitor", price: 600},
+  { id: 6, name: "Microfono", price: 40}
 ]
 
 export const useProductsStore = defineStore('products', {
-    state: () => ({ products: <Array<Product>>[] }),
+  state: () => ({ products: <Array<Product>>[] }),
 
-    actions: {
-      getProducts(){
-        if(this.products == undefined || this.products.length == 0)
-            this.products = this.products.concat(productss)      
-        return this.products;
-      }
-    },
+  actions: {
+    getProducts(){
+      if(this.products == undefined || this.products.length == 0)
+          this.products = this.products.concat(productss)      
+      return this.products;
+    }
+  },
 })
