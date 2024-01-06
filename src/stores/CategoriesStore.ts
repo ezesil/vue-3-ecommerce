db40e2a28f56cfaef407cb0d228ad4e4f03f84d7
Ezedev <ezesil96@gmail.com>
2024-01-06 19:21:26 -0300

    Movida carpeta Data de public a src

--- src/stores/CategoriesStore.ts
+++ src/stores/CategoriesStore.ts
@@ -1,14 +1,10 @@
 import { defineStore } from 'pinia'
 import type { Category } from '@/models/Category'
-
-const initialCategories: Category[] = [
-    { id: 1, name: 'Oficina', description: 'Productos de oficina.' },
-    { id: 2, name: 'Computadora', description: 'Accesorios para computadoras.' }
-]
+import categoriesData from '../data/categories.json'
 
 export const useCategoriesStore = defineStore('categories', {
     state: () => ({ 
-        _categories: initialCategories as Category[]
+        _categories: categoriesData as Category[]
     }),
     getters: {
         categories(state){
--- src/stores/ProductsStore.ts
+++ src/stores/ProductsStore.ts
@@ -3,18 +3,8 @@ import { type Product } from '../models/Product';
 import { ProductPriceSorter, ProductNameSorter, type SorterDict } from '@/models/OrderTypes';
 import { SorterConfigurator } from '../models/OrderTypes';
 import { GlobalVars } from '@/globals/globals';
+import productsData from '../data/products.json'
 
-const productss = [
-  { id: 1, name: "Silla", price: 56, categoryId: 2, image: "/products/silla1.webp"}, 
-  { id: 2, name: "Monitor", price: 450, categoryId: 1, image:"/products/monitor1.webp"},
-  { id: 3, name: "Microfono", price: 20, categoryId: 1, image:"/products/microfono1.webp"},
-  { id: 4, name: "Silla", price: 568, categoryId: 2, image:"/products/silla2.webp"}, 
-  { id: 5, name: "Monitor", price: 600, categoryId: 1, image:"/products/monitor2.webp"},
-  { id: 6, name: "Microfono", price: 40, categoryId: 1, image:"/products/microfono2.webp"},
-  { id: 7, name: "Parlantes", price: 40, categoryId: 1, image:"/products/parlantes1.webp"},
-  { id: 8, name: "Audifonos", price: 40, categoryId: 2, image:"/products/audifonos1.webp"},
-  { id: 9, name: "Mouse Gamer", price: 40, categoryId: 2, image:"/products/mousegamer1.webp"}
-] as Product[]
 
 const sorters = SorterConfigurator.createSorterDict([
   new ProductPriceSorter(),
@@ -23,7 +13,7 @@ const sorters = SorterConfigurator.createSorterDict([
 
 export const useProductsStore = defineStore('products', {
   state: () => ({
-      _products: productss as Product[],
+      _products: productsData as Product[],
       categoryId: null as Number | null,
       order: null as String | null,
       sorters: sorters as SorterDict
